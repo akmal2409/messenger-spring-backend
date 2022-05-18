8bff4c68339a418620cfdde90ac4f138d77c0629
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-05-18 19:56:21 +0200

    Initialise spring project with dependencies
    Created a spring boot project with Java 17, cassandra, kafka and other
    dependencies.

---
+++ src/main/java/com/akmal/messengerspringbackend/MessengerSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.akmal.messengerspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessengerSpringBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MessengerSpringBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/akmal/messengerspringbackend/MessengerSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akmal.messengerspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessengerSpringBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
