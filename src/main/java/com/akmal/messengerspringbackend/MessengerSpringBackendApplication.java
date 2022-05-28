d679af9b93f1ab28835041ceaa5f7d342db7ddcb
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-05-28 15:30:47 +0200

    Disabled Cassandra Autoconfiguration
    Disabled Spring Data Cassandra auto config because all the beans and properties are provided manually. Furthermore, there is no need for repositories.

--- src/main/java/com/akmal/messengerspringbackend/MessengerSpringBackendApplication.java
+++ src/main/java/com/akmal/messengerspringbackend/MessengerSpringBackendApplication.java
@@ -2,8 +2,21 @@ package com.akmal.messengerspringbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.cassandra.CassandraReactiveDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.cassandra.CassandraReactiveRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.cassandra.CassandraRepositoriesAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.cassandra.core.CassandraOperations;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    CassandraAutoConfiguration.class,
+    CassandraDataAutoConfiguration.class,
+    CassandraReactiveDataAutoConfiguration.class,
+    CassandraRepositoriesAutoConfiguration.class,
+    CassandraReactiveRepositoriesAutoConfiguration.class
+})
 public class MessengerSpringBackendApplication {
 
   public static void main(String[] args) {
