8ebe37da52d3497935c94e0b3af2bad8e222df70
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-06-03 17:54:10 +0200

    Create custom exceptions for future MessageService

---
+++ src/main/java/com/akmal/messengerspringbackend/exception/CorruptedThreadException.java
@@ -0,0 +1,32 @@
+package com.akmal.messengerspringbackend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 02/06/2022 - 18:28
+ * @project messenger-spring-backend
+ * @since 1.0
+ */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class CorruptedThreadException extends RuntimeException {
+
+  public CorruptedThreadException(String message) {
+    super(message);
+  }
+
+  public CorruptedThreadException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public CorruptedThreadException(Throwable cause) {
+    super(cause);
+  }
+
+  public CorruptedThreadException(String message, Throwable cause, boolean enableSuppression,
+      boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+}
---
+++ src/main/java/com/akmal/messengerspringbackend/exception/EntityNotFoundException.java
@@ -0,0 +1,36 @@
+package com.akmal.messengerspringbackend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 01/06/2022 - 20:57
+ * @project messenger-spring-backend
+ * @since 1.0
+ */
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class EntityNotFoundException extends RuntimeException {
+
+  public EntityNotFoundException() {
+    super("Entity with given ID was not found");
+  }
+
+  public EntityNotFoundException(String message) {
+    super(message);
+  }
+
+  public EntityNotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public EntityNotFoundException(Throwable cause) {
+    super(cause);
+  }
+
+  public EntityNotFoundException(String message, Throwable cause, boolean enableSuppression,
+      boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+}
