d6f71af2ac1c39e11624304cfc6e97ad3a976372
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-06-03 17:47:43 +0200

    Create ProjectConfigurationProperties.java that will hold global configuration properties

---
+++ src/main/java/com/akmal/messengerspringbackend/config/ProjectConfigurationProperties.java
@@ -0,0 +1,22 @@
+package com.akmal.messengerspringbackend.config;
+
+import java.time.Instant;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 01/06/2022 - 20:32
+ * @project messenger-spring-backend
+ * @since 1.0
+ */
+@Configuration
+@ConfigurationProperties(prefix = "project")
+@Getter @Setter
+public class ProjectConfigurationProperties {
+  private long customEpochMilli = Instant.EPOCH.toEpochMilli();
+  private int nodeId = -1;
+}
