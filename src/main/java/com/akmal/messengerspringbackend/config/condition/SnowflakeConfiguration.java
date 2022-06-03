6a469fcf37fef8c56b5f5dcdfa6b495102d91515
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-06-03 17:55:27 +0200

    Provide SnowflakeGenerator as bean in SnowflakeConfiguration.java
    Configured and provided SnowflakeGenerator as a singleton bean to the spring context.

---
+++ src/main/java/com/akmal/messengerspringbackend/config/condition/SnowflakeConfiguration.java
@@ -0,0 +1,28 @@
+package com.akmal.messengerspringbackend.config.condition;
+
+import com.akmal.messengerspringbackend.config.ProjectConfigurationProperties;
+import com.akmal.messengerspringbackend.snowflake.SimpleSnowflakeGenerator;
+import com.akmal.messengerspringbackend.snowflake.SnowflakeGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 01/06/2022 - 20:50
+ * @project messenger-spring-backend
+ * @since 1.0
+ */
+@Configuration
+public class SnowflakeConfiguration {
+
+  @Bean
+  SnowflakeGenerator snowflakeGenerator(ProjectConfigurationProperties projectProps) {
+    if (projectProps.getNodeId() != -1) {
+      return SimpleSnowflakeGenerator.withCustomEpochAndNodeId(projectProps.getCustomEpochMilli(),
+          projectProps.getNodeId());
+    } else {
+      return SimpleSnowflakeGenerator.withCustomEpoch(projectProps.getCustomEpochMilli());
+    }
+  }
+}
