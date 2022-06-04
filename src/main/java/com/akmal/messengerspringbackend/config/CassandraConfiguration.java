08ceb0af8f0ca6b03ea65243bf858fd765d4f773
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-06-04 15:11:34 +0200

    Change CassandraConfiguration.java in favour of AbstractCassandraConfiguration.
    Previously the mapping context never scanned the entities and hence could not create a cache for models that it was mapping from the cassandra tables. The reason was due to manual configuration and that I have missed certain beans and class loaders because they were never mentioned in the documentation. After load testing and profiling the application, the decision has been made to change
    the configuration to the more peformant one provided by Spring Boot. Furthermore, I have fine tuned the performance and added compression and overrode certain configuration props.

--- src/main/java/com/akmal/messengerspringbackend/config/CassandraConfiguration.java
+++ src/main/java/com/akmal/messengerspringbackend/config/CassandraConfiguration.java
@@ -1,33 +1,28 @@
 package com.akmal.messengerspringbackend.config;
 
 import com.akmal.messengerspringbackend.config.condition.ScriptInitCondition;
-import com.datastax.oss.driver.api.core.CqlIdentifier;
-import com.datastax.oss.driver.api.core.CqlSession;
-import java.util.stream.Collectors;
+import com.datastax.oss.driver.api.core.ConsistencyLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.cassandra.SessionFactory;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
-import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
-import org.springframework.data.cassandra.config.SchemaAction;
-import org.springframework.data.cassandra.config.SessionFactoryFactoryBean;
+import org.springframework.data.cassandra.config.AbstractSessionConfiguration;
+import org.springframework.data.cassandra.config.CompressionType;
 import org.springframework.data.cassandra.core.AsyncCassandraOperations;
 import org.springframework.data.cassandra.core.AsyncCassandraTemplate;
 import org.springframework.data.cassandra.core.CassandraOperations;
 import org.springframework.data.cassandra.core.CassandraTemplate;
 import org.springframework.data.cassandra.core.convert.CassandraConverter;
-import org.springframework.data.cassandra.core.convert.CassandraCustomConversions;
-import org.springframework.data.cassandra.core.convert.MappingCassandraConverter;
+import org.springframework.data.cassandra.core.cql.QueryOptions;
+import org.springframework.data.cassandra.core.cql.WriteOptions;
 import org.springframework.data.cassandra.core.cql.session.init.CompositeKeyspacePopulator;
 import org.springframework.data.cassandra.core.cql.session.init.ResourceKeyspacePopulator;
 import org.springframework.data.cassandra.core.cql.session.init.SessionFactoryInitializer;
-import org.springframework.data.cassandra.core.mapping.CassandraMappingContext;
-import org.springframework.data.cassandra.core.mapping.SimpleTupleTypeFactory;
-import org.springframework.data.cassandra.core.mapping.SimpleUserTypeResolver;
 
 /**
  * @author Akmal Alikhujaev
@@ -39,62 +34,90 @@ import org.springframework.data.cassandra.core.mapping.SimpleUserTypeResolver;
 @Configuration
 @Slf4j
 @RequiredArgsConstructor
-public class CassandraConfiguration {
+public class CassandraConfiguration extends AbstractCassandraConfiguration {
   private final CassandraConfigurationProperties cassandraProps;
 
   /**
-   * {@link CqlSessionFactoryBean} is favoured over plain {@link CqlSession}
-   * due to its ability to translate the exception and wrap them using common interface
-   * of {@link org.springframework.dao.DataAccessException} type
-   * @return Cqlsh session bean
+   * Global configuration of the reads from Cassandra. Consistency Level is set to LOCAL_QUORUM,
+   * which can be overridden on per-request basis. Execution profile is set to custom retries, which
+   * defines custom retry strategy, see {@link LocalDcConsistencyDowngradingRetryPolicy} for more
+   * information. Lastly, all requests by default are marked as idempotent due to counters not being
+   * used.
+   *
+   * @return {@link QueryOptions} global query configurations.
    */
-
   @Bean
-  CqlSessionFactoryBean session() {
-    final var session = new CqlSessionFactoryBean();
-    session.setContactPoints(String.join(",", cassandraProps.getSeeds()));
-    session.setKeyspaceName(this.cassandraProps.getKeyspace().getName());
-    session.setLocalDatacenter(this.cassandraProps.getDc());
-    log.info("Setting cassandra contact points {} in DC: {} and Keyspace: {}",
-        String.join(",", cassandraProps.getSeeds()),
-        this.cassandraProps.getDc(),
-        this.cassandraProps.getKeyspace().getName());
-    return session;
+  QueryOptions queryOptions() {
+    return QueryOptions.builder()
+        .consistencyLevel(ConsistencyLevel.LOCAL_QUORUM)
+        .executionProfile("custom-retries")
+        .idempotent(true)
+        .build();
   }
 
+  /**
+   * Same configuration as above {@link CassandraConfiguration#queryOptions()} but for writes.
+   *
+   * @return {@link WriteOptions} global write configurations.
+   */
   @Bean
-  public CassandraMappingContext mappingContext(CqlSession cqlSession) {
-    return new CassandraMappingContext();
+  WriteOptions writeOptions() {
+    return WriteOptions.builder()
+        .consistencyLevel(ConsistencyLevel.LOCAL_QUORUM)
+        .executionProfile("custom-retries")
+        .idempotent(true)
+        .build();
   }
 
+  /**
+   * The instance is a bean that provides common API and a high level of abstraction on top of the
+   * {@link com.datastax.oss.driver.api.core.CqlSession} used for synchronous, blocking
+   * communication.
+   *
+   * @param sessionFactory configured in {@link
+   *     AbstractSessionConfiguration#getRequiredSessionFactory()}
+   * @param converter entity converter in {@link
+   *     AbstractCassandraConfiguration#cassandraConverter()}
+   * @return {@link CassandraOperations}
+   */
   @Bean
-  CassandraConverter converter(CassandraMappingContext mappingContext, CqlSession cqlSession) {
-    final var converter = new MappingCassandraConverter(mappingContext);
-    converter.setUserTypeResolver(new SimpleUserTypeResolver(cqlSession,
-        CqlIdentifier.fromCql(this.cassandraProps.getKeyspace().getName())));
-    converter.setCodecRegistry(cqlSession.getContext().getCodecRegistry());
-    return converter;
-  }
-
-  @Bean
-  SessionFactoryFactoryBean cassandraSessionFactory(CqlSession cqlSession, CassandraConverter converter) {
-    final var sessionFactory = new SessionFactoryFactoryBean();
-    sessionFactory.setSession(cqlSession);
-    sessionFactory.setConverter(converter);
-    sessionFactory.setSchemaAction(SchemaAction.NONE);
-    return sessionFactory;
-  }
-
-  @Bean
-  CassandraOperations cassandraTemplate(SessionFactory sessionFactory, CassandraConverter converter) {
+  CassandraOperations cassandraTemplate(
+      SessionFactory sessionFactory, CassandraConverter converter) {
     return new CassandraTemplate(sessionFactory, converter);
   }
 
+  /**
+   * Same as above {@link CassandraConfiguration#cassandraTemplate(SessionFactory,
+   * CassandraConverter)}, however, AsyncOperations is for asynchronous requests between the client
+   * and the database.
+   *
+   * @param sessionFactory configured in {@link
+   *     AbstractSessionConfiguration#getRequiredSessionFactory()}
+   * @param converter entity converter in {@link
+   *     AbstractCassandraConfiguration#cassandraConverter()}
+   * @return {@link AsyncCassandraOperations}
+   */
   @Bean
-  AsyncCassandraOperations asyncCassandraOperations(SessionFactory sessionFactory, CassandraConverter converter) {
+  AsyncCassandraOperations asyncCassandraOperations(
+      SessionFactory sessionFactory, CassandraConverter converter) {
     return new AsyncCassandraTemplate(sessionFactory, converter);
   }
 
+  /**
+   * Standard Spring's resource populators adjusted for spring-data-cassandra's API. There is a
+   * support for two scripts, namely schema.cql and data.cql. Both scripts can be enabled and
+   * specified in the {@link CassandraConfigurationProperties#getData()} and {@link
+   * CassandraConfigurationProperties#getSchema()}.
+   *
+   * At the start up if one of the scripts is enabled through the the application properties then
+   * @Conditional is triggered and the bean is injected into the spring's context with the loaded
+   * scipts that are executed in the following order.
+   * 1) Schema creation
+   * 2) Data population
+   *
+   * @param sessionFactory {@link AbstractSessionConfiguration#getRequiredSessionFactory()}
+   * @return {@link SessionFactoryInitializer}
+   */
   @Bean
   @Conditional(ScriptInitCondition.class)
   SessionFactoryInitializer sessionFactoryInitializer(SessionFactory sessionFactory) {
@@ -107,9 +130,9 @@ public class CassandraConfiguration {
       schemaPopulator = new ResourceKeyspacePopulator();
       schemaPopulator.setSeparator(";");
       schemaPopulator.setScripts(
-          this.cassandraProps.getSchema()
-              .getScripts().stream()
-              .map(ClassPathResource::new).toArray(ClassPathResource[]::new));
+          this.cassandraProps.getSchema().getScripts().stream()
+              .map(ClassPathResource::new)
+              .toArray(ClassPathResource[]::new));
     }
 
     ResourceKeyspacePopulator dataPopulator = null;
@@ -119,9 +142,9 @@ public class CassandraConfiguration {
       dataPopulator.setSeparator("@@");
       dataPopulator.setScripts();
       dataPopulator.setScripts(
-          this.cassandraProps.getData()
-              .getScripts().stream()
-              .map(ClassPathResource::new).toArray(ClassPathResource[]::new));
+          this.cassandraProps.getData().getScripts().stream()
+              .map(ClassPathResource::new)
+              .toArray(ClassPathResource[]::new));
     }
 
     final var compositeKeyspacePopulator = new CompositeKeyspacePopulator();
@@ -131,5 +154,40 @@ public class CassandraConfiguration {
 
     sessionFactoryInitializer.setKeyspacePopulator(compositeKeyspacePopulator);
     return sessionFactoryInitializer;
+  }
+
+  @Override
+  protected Resource getDriverConfigurationResource() {
+    return this.cassandraProps.getDriverConfig();
+  }
+
+  @Override
+  public String[] getEntityBasePackages() {
+    return new String[] {"com.akmal.messengerspringbackend.model"};
+  }
+
+  @Override
+  protected String getKeyspaceName() {
+    return this.cassandraProps.getKeyspace().getName();
+  }
+
+  @Override
+  protected String getContactPoints() {
+    return String.join(",", this.cassandraProps.getSeeds());
+  }
+
+  @Override
+  protected String getLocalDataCenter() {
+    return this.cassandraProps.getDc();
+  }
+
+  @Override
+  protected String getSessionName() {
+    return this.cassandraProps.getClusterName();
+  }
+
+  @Override
+  protected CompressionType getCompressionType() {
+    return CompressionType.LZ4;
   }
 }
