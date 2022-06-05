6ca954167ef372430cd15085895016e4912f2d6b
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-06-05 13:35:48 +0200

    ScrollContent.java implement the response object for scroll like content
    ScrollContent differs from the Page object simply because ScrollContent doesnt have a page number or an offset but simply stores the byte representation of the paging state that is used by the Cassandra Coordinator node to fetch the records from the particular offset.

---
+++ src/main/java/com/akmal/messengerspringbackend/dto/v1/ScrollContent.java
@@ -0,0 +1,70 @@
+package com.akmal.messengerspringbackend.dto.v1;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.function.Consumer;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.springframework.data.util.Streamable;
+
+/**
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 04/06/2022 - 15:34
+ * @project messenger-spring-backend
+ * @since 1.0
+ */
+public record ScrollContent<T> (
+    String pagingState, // stores the offset from where to start to read
+    List<T> content
+) implements Streamable<T> {
+
+  public static <T> ScrollContent<T> of(@NotNull List<T> content,
+      @Nullable String pagingState) {
+    return new ScrollContent<>(pagingState, content);
+  }
+
+  @JsonIgnore
+  public static <T> ScrollContent<T> empty() {
+    return new ScrollContent<>(null, List.of());
+  }
+
+  @Contract(value = " -> new", pure = true)
+  @Override
+  public @NotNull java.util.Iterator<T> iterator() {
+    return new Iterator();
+  }
+
+  private class Iterator implements java.util.Iterator<T> {
+    private int currentIndex;
+
+    private Iterator() {
+      this.currentIndex = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+      return currentIndex < content.size() - 1;
+    }
+
+    @Override
+    public T next() {
+      if (!this.hasNext()) {
+        throw new NoSuchElementException(String.format("No element at index %d for size %d",
+            this.currentIndex, content.size()));
+      }
+
+      return content.get(this.currentIndex++);
+    }
+
+
+    @Override
+    public void forEachRemaining(Consumer<? super T> action) {
+      for (int i = this.currentIndex; i < content.size(); i++) {
+        action.accept(content.get(i));
+      }
+    }
+  }
+}
