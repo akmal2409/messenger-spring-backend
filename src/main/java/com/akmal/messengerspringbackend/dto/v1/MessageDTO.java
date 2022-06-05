7ccd161301d07a6a99a5d39216c32e4ee5b860ea
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-06-05 13:20:29 +0200

    MessageDTO.java create message dto object.

---
+++ src/main/java/com/akmal/messengerspringbackend/dto/v1/MessageDTO.java
@@ -0,0 +1,23 @@
+package com.akmal.messengerspringbackend.dto.v1;
+
+import java.util.UUID;
+
+/**
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 03/06/2022 - 19:32
+ * @project messenger-spring-backend
+ * @since 1.0
+ */
+public record MessageDTO(
+    long messageId,
+    String threadId,
+    int bucket,
+    String authorId,
+    String body,
+    boolean read,
+    boolean edited,
+    boolean systemMessage
+) {
+
+}
