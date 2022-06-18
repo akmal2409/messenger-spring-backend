9be300a275e32ce0e36bd169c4d2f48583f44098
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-06-18 17:09:51 +0200

    Implement custom AuthorizationException.java

---
+++ src/main/java/com/akmal/messengerspringbackend/exception/AuthorizationException.java
@@ -0,0 +1,31 @@
+package com.akmal.messengerspringbackend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 17/06/2022 - 19:41
+ * @project messenger-spring-backend
+ * @since 1.0
+ */
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class AuthorizationException extends RuntimeException {
+  public AuthorizationException(String message) {
+    super(message);
+  }
+
+  public AuthorizationException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public AuthorizationException(Throwable cause) {
+    super(cause);
+  }
+
+  public AuthorizationException(String message, Throwable cause, boolean enableSuppression,
+      boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+}
