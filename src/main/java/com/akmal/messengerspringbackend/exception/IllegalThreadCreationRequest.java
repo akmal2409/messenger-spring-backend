b7113940df145b5300ed0acdb070b32885d25c00
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-06-30 18:29:24 +0200

    Create IllegalThreadCreationRequest.java custom exception

---
+++ src/main/java/com/akmal/messengerspringbackend/exception/IllegalThreadCreationRequest.java
@@ -0,0 +1,35 @@
+package com.akmal.messengerspringbackend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ * The exception is thrown in case there are violations during the thread creation
+ * between the users.
+ *
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 27/06/2022 - 20:29
+ * @project messenger-spring-backend
+ * @since 1.0
+ */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class IllegalThreadCreationRequest extends RuntimeException{
+
+  public IllegalThreadCreationRequest(String message) {
+    super(message);
+  }
+
+  public IllegalThreadCreationRequest(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public IllegalThreadCreationRequest(Throwable cause) {
+    super(cause);
+  }
+
+  public IllegalThreadCreationRequest(String message, Throwable cause, boolean enableSuppression,
+      boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+}
