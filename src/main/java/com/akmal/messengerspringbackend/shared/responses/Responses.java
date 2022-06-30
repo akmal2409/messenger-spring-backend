eb4e7b18279765cff7a50f67da95cc46e5fe53de
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-06-30 18:30:25 +0200

    Create a utility class Responses.java to wrap responses into ResponseEntities.
    if the collection is empty or an item is null, it will automatically return HTTP header No Content.

---
+++ src/main/java/com/akmal/messengerspringbackend/shared/responses/Responses.java
@@ -0,0 +1,27 @@
+package com.akmal.messengerspringbackend.shared.responses;
+
+import java.util.Collection;
+import java.util.Optional;
+import org.springframework.http.ResponseEntity;
+
+/**
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 27/06/2022 - 20:11
+ * @project messenger-spring-backend
+ * @since 1.0
+ */
+public final class Responses {
+
+  public static <T> ResponseEntity<T> wrap(T item) {
+    return Optional.ofNullable(item)
+               .map(ResponseEntity::ok)
+               .orElse(ResponseEntity.noContent().build());
+  }
+
+  public static <T> ResponseEntity<Collection<T>> wrap(Collection<T> collection) {
+    return Optional.ofNullable(collection)
+        .map(c -> c.isEmpty() ? null : ResponseEntity.ok(c))
+        .orElse(ResponseEntity.noContent().build());
+  }
+}
