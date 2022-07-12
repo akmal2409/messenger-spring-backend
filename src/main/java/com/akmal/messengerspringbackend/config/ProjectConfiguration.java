5e632d8dec9262207a96dae3b96b453e11699936
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-07-12 19:55:31 +0200

    Add AsyncTaskExecutor bean to ProjectConfiguration.java

--- src/main/java/com/akmal/messengerspringbackend/config/ProjectConfiguration.java
+++ src/main/java/com/akmal/messengerspringbackend/config/ProjectConfiguration.java
@@ -3,6 +3,8 @@ package com.akmal.messengerspringbackend.config;
 import com.akmal.messengerspringbackend.shared.timeago.TimeAgoConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 /**
  * @author Akmal Alikhujaev
@@ -17,5 +19,16 @@ public class ProjectConfiguration {
   @Bean
   TimeAgoConverter timeAgoConverter() {
     return TimeAgoConverter.withDefaults();
+  }
+
+  @Bean
+  public AsyncTaskExecutor asyncExecutor() {
+    final var executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(7);
+    executor.setMaxPoolSize(40);
+    executor.setQueueCapacity(11);
+    executor.setThreadNamePrefix("async-executor-");
+    executor.initialize();
+    return executor;
   }
 }
