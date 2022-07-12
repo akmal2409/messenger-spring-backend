90d762a65f88e46e92becac2cea3ec186afeebb7
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-07-12 19:57:19 +0200

    Secure user destinations for websocket connections.

--- src/main/java/com/akmal/messengerspringbackend/config/security/WebsocketSecurityConfiguration.java
+++ src/main/java/com/akmal/messengerspringbackend/config/security/WebsocketSecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.akmal.messengerspringbackend.config.security;
 
 import com.akmal.messengerspringbackend.config.websocket.WebSocketConfiguration;
+import com.akmal.messengerspringbackend.websocket.controller.WsMessageController;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
@@ -15,15 +16,17 @@ import org.springframework.security.config.annotation.web.socket.AbstractSecurit
 @Configuration
 public class WebsocketSecurityConfiguration
     extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+  public static final String WS_API_DEST_PREFIX = "/ws-api";
 
   @Override
   protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
     messages
         .simpDestMatchers("/queue/**", WebSocketConfiguration.THREAD_TOPIC)
         .denyAll()
-        .simpDestMatchers(WebSocketConfiguration.THREAD_TOPIC + "/{threadId}")
+        .simpDestMatchers(WebSocketConfiguration.THREAD_TOPIC.concat("/{threadId}/**"), WS_API_DEST_PREFIX.concat("/users/{userId}/threads/{threadId}/**"))
         .access(
             "@userService.currentUser.threadIds.contains(T(java.util.UUID).fromString(#threadId))")
+        .simpDestMatchers(WS_API_DEST_PREFIX.concat("/users/{userId}/**")).access("authentication.name == #userId")
         .anyMessage()
         .authenticated();
   }
